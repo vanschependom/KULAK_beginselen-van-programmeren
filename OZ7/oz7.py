0686f8b94ca4b2d042a20fabb351c1af1217adc7
Vincent Van Schependom <vincent.van.schependom@gmail.com>
2023-11-21 16:05:15 +0100

    OZ7 in de les

---
+++ OZ7/oz7.py
@@ -0,0 +1,2 @@
+buurmatrix = [[0, 1, 2, 1, None, None, None], [1, 0, 1, None, 5, None, None], [2, 1, 0, None, 3, 1, None], [1, None, None,
+                                                                                                            0, None, 5, None], [None, 5, 3, 0, None, None, 1], [None, None, None, 5, None, 0, 2], [None, None, None, None, 1, 2, 0]]
